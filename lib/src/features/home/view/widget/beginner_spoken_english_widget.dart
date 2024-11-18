d5587da2d3429006b677dab855487820865a73b4
saeid721 <saeid721km@gmail.com>
2024-11-18 14:44:49 +0600

    update

--- lib/src/features/home/view/widget/beginner_spoken_english_widget.dart
+++ lib/src/features/home/view/widget/beginner_spoken_english_widget.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../../../video_details/view/beginner_spoken_english_video_details_screen.dart';
-import '../../../video_details/view/spoken_english_practice_video_details_screen.dart';
-import '../../../video_details/view/video_details_screen.dart';
 import '../../controller/home_controller.dart';
 import '../../../../global/widget/global_sized_box.dart';
 import '../components/beginner_spoken_english_see_all_screen.dart';
--- lib/src/features/home/view/widget/spoken_english_practice_english_widget.dart
+++ lib/src/features/home/view/widget/spoken_english_practice_english_widget.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../../../video_details/view/spoken_english_practice_video_details_screen.dart';
-import '../../../video_details/view/video_details_screen.dart';
 import '../../controller/home_controller.dart';
 import '../../../../global/widget/global_sized_box.dart';
 import '../components/spoken_english_practice_see_all_screen.dart';
@@ -46,7 +45,7 @@ class _SpokenEnglishPracticeWidgetState extends State<SpokenEnglishPracticeWidge
                         thumbnail: recent.thumbnail ?? "",
                         title: recent.title ?? "",
                         onTap: () {
-                          Get.to(() => VideoDetailsScreen(
+                          Get.to(() => SpokenEnglishPracticeVideoDetailsScreen(
                                 id: recent.id.toString(),
                                 title: recent.title ?? "",
                                 shortDescription: recent.shortDescription ?? "",
