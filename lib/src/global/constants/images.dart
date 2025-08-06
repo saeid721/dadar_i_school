5ca70cad1e4fd33b61e6b6e1ac1477180dbcb548
saeid721 <saeid721km@gmail.com>
2025-08-06 16:25:21 +0600

    update

--- lib/src/global/constants/images.dart
+++ lib/src/global/constants/images.dart
@@ -10,12 +10,6 @@ class Images {
   static const String share = 'assets/app_src/icon/share.png';
   static const String contact = 'assets/app_src/icon/contact.png';
 
-  static const String close = 'assets/app_src/icon/close.png';
-  static const String scanner = 'assets/main_src/icons/scanner.png';
-  static const String eye1 = 'assets/app_src/icon/eye1.png';
-  static const String eye2 = 'assets/app_src/icon/eye2.pngg';
-
-
   //==============/@ App Img
   static const String textUre = 'assets/app_src/texture.jpg';
 
@@ -23,30 +17,10 @@ class Images {
   static const String playBuIc = 'assets/app_src/icon/play_button_ic.png';
 
   //==/@ App Src Icon
-  static const String homeIc = 'assets/app_src/icon/home_ic.png';
-  static const String searchIc = 'assets/app_src/icon/search_ic.png';
-  static const String downloadIc = 'assets/app_src/icon/download_ic.png';
-  static const String accountIc = 'assets/app_src/icon/account_ic.png';
-  static const String logInIc = 'assets/app_src/icon/login_ic.png';
-  static const String languageIc = 'assets/app_src/icon/language.png';
-  static const String watchIc = 'assets/app_src/icon/watching.png';
-  static const String deleteIc = 'assets/app_src/icon/delete_ic.png';
-  static const String shareIc = 'assets/app_src/icon/share_ic.png';
-  static const String shareDetailsIc = 'assets/app_src/icon/share_details_ic.png';
-  static const String watchListLaterIc = 'assets/app_src/icon/watch_list_later_ic.png';
-  static const String pushIc = 'assets/app_src/icon/push_ic.png';
-  static const String reverseIc = 'assets/app_src/icon/reverse_ic.png';
   static const String nextIc = 'assets/app_src/icon/next_ic.png';
-  static const String previousIc = 'assets/app_src/icon/previous_ic.png';
+  static const String homeIc = 'assets/app_src/icon/home_ic.png';
+  static const String reverseIc = 'assets/app_src/icon/reverse_ic.png';
   static const String playSpeedIc = 'assets/app_src/icon/play_speed_ic.png';
-  static const String quantityIc = 'assets/app_src/icon/quantity_ic.png';
-  static const String logOutIc = 'assets/app_src/icon/logout_ic.png';
-  static const String activatedTvIc = 'assets/app_src/icon/activated_tv.png';
-  static const String deviceManagementIc = 'assets/app_src/icon/device_management_ic.png';
-  static const String billingHistoryIc = 'assets/app_src/icon/billing_history_ic.png';
-  static const String addIc = 'assets/app_src/icon/add_ic.png';
-  static const String emptyIc = 'assets/app_src/icon/empty_ic.png';
-  static const String checkIc = 'assets/app_src/icon/check.png';
 
   //==/@ Social
   static const String facebookInc = 'assets/app_src/icon/social_ic/facebook.png';
